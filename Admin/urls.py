a8915d276726091db763972041f677fbc639b356
Vivek <vbreaker721@gmail.com>
2022-04-15 01:08:34 +0530

    third

--- Admin/urls.py
+++ Admin/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('admin-index/',views.admin_index,name='admin-index'),
-    path('admin-login',views.admin_login,name='admin-login'),
-    path('admin-logout',views.admin_logout,name='admin-logout'),
+    path('admin-index/', views.admin_index,name='admin-index'),
+    path('admin-login/',views.admin_login,name='admin-login'),
 ]
--- Admin/views.py
+++ Admin/views.py
@@ -6,22 +6,8 @@ from .models import *
 # Create your views here.
 
 def admin_index(request):
-    adi=Admin.objects.get(user_id=request.session['user_id'])
-    return render(request,'admin-index.html',{'adi':adi})
+    return render(request,'admin-header.html')
 
 def admin_login(request):
-    if request.method == 'POST':
-        try:
-            adi=Admin.objects.get(user_id=request.POST['user_id'])
-            if request.POST['password'] == adi.password:
-                request.session['user_id'] = request.POST['user_id']
-                return redirect('admin-index')
-            return render(request,'admin-login.html',{'msg':'Incorrect Password'})
-        except:
-            msg = 'User ID not registered' 
-            return render(request,'admin-login.html',{'msg':msg})
     return render(request,'admin-login.html')
 
-def admin_logout(request):
-    del request.session['user_id']
-    return redirect('index')
